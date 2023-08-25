3c323ba4ec0202a1571f8402a2644d3619cc4176
whiterain7 <makmaks02@mail.ru>
2023-08-25 19:19:49 +0300

    front

---
+++ front/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="container">
+    <header id="header">
+
+    </header>
+
+    <main id="main">
+      <form>
+        <section class="slide">
+          <hgroup>
+
+          </hgroup>
+
+
+        </section>
+      </form>
+    </main>
+
+    <footer id="footer">
+
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'indexPage',
+
+    data: function () {
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
